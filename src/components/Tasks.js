dce631ad09de5a141ba11aa5575bbf762fc45113
Sujan <sujan.maharjan@gurzu.com>
2022-05-27 08:48:46 +0545

    fixed router issue

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,18 @@
 import Task from "./Task";
+import Header from "./Header";
+import Footer from "./Footer";
+import React, { useState } from "react";
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
+  const [showAddTask, setShowAddTask] = useState(false);
+
   return (
     <>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+
       {tasks.map((task) => (
         <Task
           key={task.id}
@@ -11,6 +21,8 @@ const Tasks = ({ tasks, onDelete, onToggle }) => {
           onToggle={onToggle}
         />
       ))}
+
+      <Footer />
     </>
   );
 };
